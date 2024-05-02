8fd15719563df5fabb00cfc9cef0995cded4fd5d
leejohnli <li_leejohn@yahoo.com>
2024-05-02 17:56:32 +0800

    added customer6 table

---
+++ dbo/Tables/Customer6.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Customer6]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [name] NCHAR(10) NULL, 
+    [address] NCHAR(10) NULL, 
+    [birthdate] NCHAR(10) NULL, 
+    [age] NCHAR(10) NULL
+)
