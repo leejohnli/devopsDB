086cb28ca835ffcd4af02bd62e1f23f96c3a109d
leejohnli <li_leejohn@yahoo.com>
2024-05-02 16:20:54 +0800

    additonal table Customer2

---
+++ Customer2.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Customer2]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [name] VARCHAR(50) NULL, 
+    [address] VARBINARY(MAX) NULL, 
+    [zip_code] INT NULL, 
+    [postal_code] INT NULL
+)
