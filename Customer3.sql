21da69b3e00cc81ca39c93841799016243bb30a8
leejohnli <li_leejohn@yahoo.com>
2024-05-02 17:02:15 +0800

    add table Custome3

---
+++ Customer3.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Customer3]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [firstname] NCHAR(10) NULL, 
+    [middlename] NCHAR(10) NULL, 
+    [lastname] NCHAR(10) NULL, 
+    [birthdate] NCHAR(10) NULL, 
+    [age] NCHAR(10) NULL, 
+    [address] NCHAR(10) NULL
+)
