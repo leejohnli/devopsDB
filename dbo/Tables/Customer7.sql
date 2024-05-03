2b94169db56053608b329e82582c9e0957e21c39
leejohnli <li_leejohn@yahoo.com>
2024-05-03 12:07:23 +0800

    additonal table customer7

---
+++ dbo/Tables/Customer7.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Customer7]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [name] NCHAR(10) NULL, 
+    [age] NCHAR(10) NULL, 
+    [birthdate] NCHAR(10) NULL, 
+    [address] NCHAR(10) NULL
+)
